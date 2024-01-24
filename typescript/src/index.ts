07fa319d8cd215f762cb59c5babff85a600d6e6f
Andrew923 <yuandrew923@gmail.com>
2024-01-24 12:32:47 -0500

    updated renderer to handle any Shape object

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,6 @@
-import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { newRenderer } from './renderer.js'
+import { rectangle } from './shapes/rectangle.js'
 
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw();
+const rect: Shape = rectangle(2, 3)
+const renderer = newRenderer(rect)
+renderer.draw()
